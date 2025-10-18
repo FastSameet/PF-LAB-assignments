d272af1b3f2fd8f65a4c147417b7b3a92297c91c
FastSameet <k250857@nu.edu.pk>
2025-10-18 21:47:22 +0500

    Update question5.c

--- lab task 7/question5.c
+++ lab task 7/question5.c
@@ -1,7 +1,10 @@
 #include<Stdio.h>
 int main(){
 	int arr[12];
-	int i, j, num, size=12;
+	int newarr[12];
+	int i, num, size=12;
+	int newsize=0;
+	
 	for(i=0;i<size;i++){
 		printf("enter 12 numbers:");
 		scanf("%d",&arr[i]);
@@ -9,17 +12,14 @@ int main(){
 		printf("enter the number you want to remove:");
 		scanf("%d",&num);
 	for(i=0;i<size;i++){
-		if(arr[i]==num){
-			for(j=i;j<size-1;j++){
-				arr[j]=arr[j+1];
-			}
-			size--;
-			i--;
-		}
+	 		if(arr[i]!=num){
+	 			newarr[newsize]=arr[i];
+	 			newsize++;
+			 }
 	}
 		printf("after removing %d:",num);
-		 for(i=0;i<size;i++){
-		 	printf("%d",arr[i]);
+		 for(i=0;i<newsize;i++){
+		 	printf("%d",newarr[i]);
 		 }	
 return 0;	
 }
